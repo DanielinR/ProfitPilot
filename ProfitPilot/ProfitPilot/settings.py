58ed86a4ec2efa496c887d18a9439a643306b760
Danielin <danielxroh@gmail.com>
2023-11-14 16:20:39 +0100

    correct static root

--- ProfitPilot/ProfitPilot/settings.py
+++ ProfitPilot/ProfitPilot/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/profit_pilot/static/'
+STATIC_ROOT = '/var/www/profit_pilot/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
