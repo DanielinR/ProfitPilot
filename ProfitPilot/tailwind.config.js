e8b06414371dec754d64f847293b759e6e9edd70
Danielin <danielxroh@gmail.com>
2024-02-08 21:44:38 +0100

    install tailwind

---
+++ ProfitPilot/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './ProfitPilotApp/templates/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
